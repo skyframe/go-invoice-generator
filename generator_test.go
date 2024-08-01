6df76c1f00c382d34dc780bb5be0e2070a03f89b
Erick <erick@meshtrade.co>
2024-08-01 22:37:27 +0200

    chore: add extra unit test

--- generator_test.go
+++ generator_test.go
@@ -2,7 +2,9 @@ package generator
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -97,6 +99,139 @@ func TestNew(t *testing.T) {
 		Name:     "Test",
 		UnitCost: "99876.89",
 		Quantity: "2",
+		Tax: &Tax{
+			Amount: "89",
+		},
+		Discount: &Discount{
+			Percent: "30",
+		},
+	})
+
+	doc.AppendItem(&Item{
+		Name:     "Test",
+		UnitCost: "3576.89",
+		Quantity: "2",
+		Discount: &Discount{
+			Percent: "50",
+		},
+	})
+
+	doc.AppendItem(&Item{
+		Name:     "Test",
+		UnitCost: "889.89",
+		Quantity: "2",
+		Discount: &Discount{
+			Amount: "234.67",
+		},
+	})
+
+	doc.SetDefaultTax(&Tax{
+		Percent: "10",
+	})
+
+	// doc.SetDiscount(&Discount{
+	// Percent: "90",
+	// })
+	doc.SetDiscount(&Discount{
+		Amount: "1340",
+	})
+
+	pdf, err := doc.Build()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	err = pdf.OutputFileAndClose("out.pdf")
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+var (
+	_muted   = []int{244, 244, 245}
+	_primary = []int{251, 187, 24}
+)
+
+func TestMIW(t *testing.T) {
+	doc, err := New(Invoice, &Options{
+		CurrencySymbol:        "R",
+		CurrencyThousand:      ",",
+		DarkBgColor:           _primary,
+		GreyBgColor:           _muted,
+		HeaderBgColor:         _primary,
+		TextVersionTitle:      "VAT Number",
+		TextRefTitle:          "Order Number",
+		TextItemsTotalHTTitle: "Total excl. VAT",
+		TextItemsTaxTitle:     "VAT",
+		TextTotalTax:          "Total VAT",
+		TextTotalWithTax:      "Total with VAT",
+		TextItemsTaxSubscript: false,
+		TextPaymentTermTitle:  "Payment Status",
+	})
+
+	if err != nil {
+		t.Fatalf("got error %v", err)
+	}
+
+	doc.SetHeader(&HeaderFooter{
+		Text:       "<center>Made in Workshop</center>",
+		Pagination: true,
+	})
+
+	doc.SetFooter(&HeaderFooter{
+		Text:       "<center>Made in Workshop</center>",
+		Pagination: true,
+	})
+
+	doc.SetRef(fmt.Sprintf("%06d", 36))
+	doc.SetVersion("VAT123456789")
+
+	logoBytes, _ := os.ReadFile("./example_logo.png")
+	doc.SetCompany(&Contact{
+		Name: "Test Company",
+		Logo: logoBytes,
+		Address: &Address{
+			Address:    "89 Rue de Brest",
+			Address2:   "Appartement 2",
+			PostalCode: "75000",
+			City:       "Paris",
+			Country:    "France",
+		},
+		AddtionnalInfo: []string{"Cupcake: ipsum dolor"},
+	})
+
+	doc.SetCustomer(&Contact{
+		Name: "Test Customer",
+		Address: &Address{
+			Address:    "89 Rue de Paris",
+			PostalCode: "29200",
+			City:       "Brest",
+			Country:    "France",
+		},
+		AddtionnalInfo: []string{
+			"Cupcake: ipsum dolor",
+			"Cupcake: ipsum dolo r",
+		},
+	})
+
+	for i := 0; i < 10; i++ {
+		doc.AppendItem(&Item{
+			Name:        "Cupcake ipsum dolor sit amet bonbon, coucou bonbon lala jojo, mama titi toto",
+			Description: "Cupcake ipsum dolor sit amet bonbon, Cupcake ipsum dolor sit amet bonbon, Cupcake ipsum dolor sit amet bonbon",
+			UnitCost:    "99876.89",
+			Quantity:    strconv.Itoa(i + 1),
+			Tax: &Tax{
+				Percent: "20",
+				Amount:  "39975.38",
+			},
+		})
+	}
+
+	doc.AppendItem(&Item{
+		Name:     "Test",
+		UnitCost: "99876.89",
+		Quantity: "1",
 		Tax: &Tax{
 			Amount: "89",
 		},
